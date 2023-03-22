61007ede32b37a31ed88ac45ffb36e474e923b3a
Wes Hendley <wrhendley@gmail.com>
2023-03-22 10:33:08 -0500

    Create initial operate function

---
+++ code.js
@@ -0,0 +1,12 @@
+let operand1, operand2, operator;
+
+function operate(operator, op1, op2){
+    switch(operator){
+        case '+': return op1 + op2; break;
+        case '-': return op1 - op2; break;
+        case '*': return op1 * op2; break;
+        case '/': return op1 / op2; break;
+    }
+}
+
+console.log(+,3,7);
