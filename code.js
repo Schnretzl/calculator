66e3b81240ba7507380737bdbcfc33181a4b9788
Wes Hendley <wrhendley@gmail.com>
2023-03-23 10:13:28 -0500

    Fix functionality of operator buttons

--- code.js
+++ code.js
@@ -9,10 +9,22 @@ function operate(operator, op1, op2){
     }
 }
 
-let test = document.querySelectorAll('.number');
-for(let btn in test){
-    test[btn].addEventListener('click', () => {
-        currentTotal = (currentTotal * 10) + 1;
-        console.log(currentTotal);
+let inputNum = document.querySelectorAll('.number');
+for(let i = 0; i < 10; i++){
+    inputNum[i].addEventListener('click', () => {
+        if(operator === null){
+            operand1 = (operand1 * 10) + parseInt(inputNum[i].textContent);
+        }else{
+            operand2 = (operand2 * 10) + parseInt(inputNum[i].textContent);        
+        }
+        // console.log(operand1);
+    });
+}
+
+let inputOperator = document.querySelectorAll('.operator');
+for(let i = 0; i < 4; i++){
+    inputOperator[i].addEventListener('click', () => {
+        operator = inputOperator[i].textContent;
+        console.log(operator);
     });
 }
